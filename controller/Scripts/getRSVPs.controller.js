b56d34523b945274a22b37756853b458d24faf63
ReyLeal <reinaldo.leal001@gmail.com>
2020-01-01 13:21:22 -0500

    working on better validation for max guest count

--- controller/Scripts/getRSVPs.controller.js
+++ controller/Scripts/getRSVPs.controller.js
@@ -1,21 +1,29 @@
 const mongoose = require('mongoose');
 const RSVPCodes = require('../../model/RSVPCodes.model');
+const RSVPs = require('../../model/RSVPs.model');
 const getSecret = require('../../secrets.js');
 
 mongoose.connect(getSecret('dbUri'));
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-async function getCount (rsvpCode){
-  let count = 0;
+async function getCount(rsvpCode) {
+  let resp = 0;
 
-  await RSVPCodes.find({rsvpCode}, (err, response) => {
-    if(response && response[0] && response[0].count) {
-      count = response[0].count;
-    }
-  });
-
-  return count;
-};
+  if (rsvpCode === 'SHOW_ALL_RSVPS_SUNJAMIN') {
+    await RSVPs.find({'attending': true}, 'firstName lastName guestCount message', (err, response) => {
+      if (response && response[0]) {
+        resp = response;
+      }
+    });
+  } else {
+    await RSVPCodes.find({rsvpCode}, (err, response) => {
+      if (response && response[0] && response[0].count) {
+        resp = response[0].count;
+      }
+    });
+  }
+  return resp;
+}
 
 module.exports = getCount;
